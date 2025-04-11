dfcc79f016c2338ff9a541a5c81f6bc15501e17c
Your Nameartmazloev <a.mazloev@gmail.com>
2025-04-11 18:04:53 +0300

    start_project_settings

---
+++ gendiff/cli.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output', default='stylish')
+
+    args = parser.parse_args()
+    print(f'{args.first_file=} {args.second_file=} {args.format=}')
+    return args
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+from gendiff.cli import parse_args
+
+def main():
+    '''
+    Точка входа в приложения
+    '''
+    parse_args()
+    
+if __name__ == "__main__":
+    main()
+
+
+
